b1bac5df13fc0f2788fbdc1cd15be18aa44b301a
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 18:10:19 +0600

    album list added

--- musicians/views.py
+++ musicians/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from musicians.form.album import AlbumForm
+from musicians.form.musician import MusicianForm
+from musicians.models.album import Album
 from musicians.models.musician import Musician
 
 
@@ -10,3 +13,8 @@ def index(args):
 def musician_list(request):
     musicians = Musician.objects.all()
     return render(request, "musician_list.html", {"musicians": musicians})
+
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, "album_list.html", {"albums": albums})
